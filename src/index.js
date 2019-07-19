c0cc2d72358d781585e7807b9828de856c4314ee
Rosano Coutinho <pub@xeari.com>
2019-07-19 11:06:33 -0400

    Import index

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import { createFilter } from 'rollup-pluginutils';
+import MagicString from 'magic-string';
+
+import * as OLSKInternational from 'OLSKInternational';
+
+import globPackage from 'glob';
+import pathPackage from 'path';
+import jsYAMLPackage from 'js-yaml';
+
+import { ExtractOLSKLocalized } from './main.js';
+
+export default function i18nPlugin( options = {} ) {
+  const filter = createFilter( options.include, options.exclude );
+  const sourceMap = options.sourceMap !== false;
+
+  const replaceToken = '{"PLUGIN_ALFA_SEARCH_REPLACE":"PLUGIN_ALFA_SEARCH_REPLACE"}';
+  const baseDirectory = options.baseDirectory;
+  let matchedContstants = [];
+
+  return {
+		name: 'i18n',
+
+		transform(code, id) {
+			if (id.match('node_modules')) {
+				return null;
+			}
+
+			if (!filter(id)) {
+				return null;
+			}
+			
+			ExtractOLSKLocalized(code).forEach(function (e) {
+				if (matchedContstants.indexOf(e) !== -1) {
+					return;
+				}
+
+				matchedContstants.push(e);
+			})
+
+			return null;
+		},
+
+		renderChunk(code, chunk, options) {
+			let startIndex = code.indexOf(replaceToken);
+
+			if (startIndex === -1) return null;
+
+			if (!baseDirectory) {
+				throw new Error('missing options.baseDirectory');
+			}
+
+			let magicString = new MagicString(code);
+
+			magicString.overwrite(startIndex, startIndex + replaceToken.length, JSON.stringify(globPackage.sync('*i18n*.y*(a)ml', {
+			  matchBase: true,
+			  cwd: baseDirectory,
+			}).filter(function(e) {
+			  return OLSKInternational.OLSKInternationalInputDataIsTranslationFileBasename(pathPackage.basename(e));
+			}).reduce(function(coll, item) {
+				let languageID = OLSKInternational.OLSKInternationalLanguageIDForTranslationFileBasename(pathPackage.basename(item));
+				let allTranslations = jsYAMLPackage.safeLoad(require('fs').readFileSync(pathPackage.join(baseDirectory, item), 'utf8'));
+
+				return (coll[languageID] = Object.assign(coll[languageID] || {}, matchedContstants.reduce(function (coll, item) {
+					if (!allTranslations[item]) {
+						return coll;
+					}
+					return (coll[item] = allTranslations[item]) && coll;
+				}, {}))) && coll;
+			}, {})));
+
+			return {
+				code: magicString.toString(),
+				map: sourceMap ? magicString.generateMap() : null,
+			};
+					
+		},
+  };
+}
