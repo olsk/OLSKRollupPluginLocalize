b53db2347aa835640f69218d3800f047bf6affba
Rosano Coutinho <pub@xeari.com>
2019-07-20 09:39:45 -0400

    Spec OLSKRollupI18NReplaceInternationalizationToken

--- src/main-tests.js
+++ src/main-tests.js
@@ -22,3 +22,54 @@ describe('OLSKRollupI18NExtractOLSKLocalizedIdentifiers', function testOLSKRollu
 	});
 
 });
+
+describe('OLSKRollupI18NReplaceInternationalizationToken', function OLSKRollupI18NReplaceInternationalizationToken() {
+	it('throws if param1 not object', function () {
+		throws(function () {
+			mainModule.OLSKRollupI18NReplaceInternationalizationToken(null, {});
+		}, /OLSKErrorInputInvalid/);
+	});
+
+	it('throws if param1 without object.code', function () {
+		throws(function () {
+			mainModule.OLSKRollupI18NReplaceInternationalizationToken({}, {});
+		}, /OLSKErrorInputInvalid/);
+	});
+
+	it('throws if param2 not object', function () {
+		throws(function () {
+			mainModule.OLSKRollupI18NReplaceInternationalizationToken({
+			code: 'alfa',
+		}, null);
+		}, /OLSKErrorInputInvalid/);
+	});
+
+	it('returns input', function() {
+		deepEqual(mainModule.OLSKRollupI18NReplaceInternationalizationToken({
+			code: 'alfa',
+		}, {}), {
+			code: 'alfa',
+		});
+	});
+
+	it('outputs map if specified', function() {
+		deepEqual(mainModule.OLSKRollupI18NReplaceInternationalizationToken({
+			code: 'alfa',
+			map: 'bravo',
+		}, {}), {
+			code: 'alfa',
+			map: 'bravo',
+		});
+	});
+
+	it('replaces token', function() {
+		deepEqual(mainModule.OLSKRollupI18NReplaceInternationalizationToken({
+			code: mainModule.OLSKRollupI18NInternationalizationToken,
+		}, {
+			alfa: 'bravo',
+		}), {
+			code: "JSON.parse(`{\"alfa\":\"bravo\"}`)",
+		});
+	});
+
+});
--- src/main.js
+++ src/main.js
@@ -9,3 +9,34 @@ export const OLSKRollupI18NExtractOLSKLocalizedIdentifiers = function(inputData)
 		return e.replace('OLSKLocalized', '').replace(/\W/g, '');
 	});
 };
+
+import MagicString from 'magic-string';
+export const OLSKRollupI18NInternationalizationToken = 'JSON.parse(`{"PLUGIN_ALFA_SEARCH_REPLACE":"PLUGIN_ALFA_SEARCH_REPLACE"}`)';
+
+export const OLSKRollupI18NReplaceInternationalizationToken = function(param1, param2) {
+	if (typeof param1 !== 'object' || param1 === null) {
+		throw new Error('OLSKErrorInputInvalid');
+	}
+
+	if (typeof param1.code !== 'string') {
+		throw new Error('OLSKErrorInputInvalid');
+	}
+
+	if (typeof param2 !== 'object' || param2 === null) {
+		throw new Error('OLSKErrorInputInvalid');
+	}
+
+	let startIndex = param1.code.indexOf(OLSKRollupI18NInternationalizationToken);
+
+	if (startIndex === -1) return param1;
+
+	let magicString = new MagicString(param1.code);
+
+	magicString.overwrite(startIndex, startIndex + OLSKRollupI18NInternationalizationToken.length, `JSON.parse(\`${ JSON.stringify(param2) }\`)`);
+
+	return Object.assign(param1, {
+		code: magicString.toString(),
+	}, param1.map ? {
+		map: magicString.generateMap(),
+	} : {});
+};
