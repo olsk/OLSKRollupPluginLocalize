f318f78780a08e91bb4c2922bcd24f0b411d87a4
Rosano Coutinho <pub@xeari.com>
2020-10-25 11:05:37 -0300

    Purge OLSKInternationalFileDelegateDirectory

--- rollup-plugin.js
+++ rollup-plugin.js
@@ -93,10 +93,9 @@ module.exports = function i18nPlugin( options = {} ) {
 				code: code,
 				map: mod._DataHasSourceMap || options.sourceMap || options.sourcemap,
 			}, require('OLSKInternational')._OLSKInternationalPaths({
-				OLSKInternationalFileDelegateDirectory: mod._DataBaseDirectory,
 				OLSKInternationalFileDelegateGlobSync: require('glob').sync,
 				OLSKInternationalFileDelegateYAMLRead: require('js-yaml').safeLoad,
-			}).reduce(function(coll, item) {
+			}, mod._DataBaseDirectory).reduce(function(coll, item) {
 				const languageID = require('OLSKInternational').OLSKInternationalLanguageID(require('path').basename(item));
 				const data = require('js-yaml').safeLoad(require('fs').readFileSync(item, 'utf8'));
 				const includeAllData = chunk.facadeModuleId && item.match(require('path').dirname(chunk.facadeModuleId));
